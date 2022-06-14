11809396a13e66f7b3a415ba9a26fd6774861d3f
Erik Saylor <saylor.erik@gmail.com>
2022-06-14 14:56:53 -0600

    Made a login button

--- Login.js
+++ Login.js
@@ -1,7 +1,10 @@
 import {useState} from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const Login = () => {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
   const [phoneNumber, setPhoneNumber] = useState("");
   const [oneTimePassword, setOneTimePassword] = useState(null);
 
@@ -12,15 +15,23 @@ const Login = () => {
         onChangeText={setPhoneNumber}
         value={phoneNumber}
         placeholder = "360-984-1155"
+        placeholderTextColor = "#add8e6"
       />
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
         placeholder="1234"
+        placeholderTextColor = "#add8e6"
         keyboardType="numeric"
         secureTextEntry = {true}
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{console.log("Login button was clicked")}}
+      >
+        <Text>Login</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -34,6 +45,20 @@ const styles = StyleSheet.create({
   },
   mainView:{
     marginTop:100
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
   }
 });
 
